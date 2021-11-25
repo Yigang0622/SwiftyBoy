c75d9acda26074d6841b63a598352c4e7f130aa5
Yigang Zhou <zhou@zygmail.com>
2021-11-25 21:43:46 +0800

    Initial Commit

---
+++ SwiftyBoy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyBoy
+//
+//  Created by Yigang Zhou on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
